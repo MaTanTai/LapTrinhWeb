9197645f02a59970515b68b499cb9617fcdf72bd
MaTanTai <1822040764@dntu.edu.vn>
2024-08-27 10:47:40 +0700

    tao them baitap

---
+++ BaiTap03/BaiTap03/Controllers/TheLoaiController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap03.Controllers
+{
+    public class TheLoaiController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
